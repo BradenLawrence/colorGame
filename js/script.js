9999a2d5bdbf2624af6ab5dcbe648c40382e74f6
Braden Lawrence <lawrencebradenm@gmail.com>
2017-02-06 23:39:10 -0500

    Color buttons now log whether they are the correct or incorrect answer.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
+// Array that will hold our onscreen boxes
 let box = []
 
-
-const boxClicked = function(e) {
-  console.log("Hi, this is " + this.name + "!")
+// Click handler for when an onscreen box is clicked
+const boxClicked = function() {
+  console.log(this.name + ": " + this.color())
+  console.log("Target: " + myTargetNum.color())
+  if(this.color() === myTargetNum.color()) {
+    console.log("Correct!")
+  } else {
+    console.log("Incorrect!")
+  }
 }
 
-const colorButton = function(name, id, display) {
-    let _this = this
+// Function to let us select a box at random.
+// Will be used to turn one of the boxes into the correct answer.
+const randomBox = function(array) {
+    let num = Math.floor(Math.random() * array.length)
+    return array[num]
+}
+
+// Definition for the color boxes
+const colorButton = function(name, display) {
     this.name = name
-    this.id = id
     this.display = display
     this.red = null
     this.green = null
@@ -16,11 +29,14 @@ const colorButton = function(name, id, display) {
     this.color = function() {
         return "rgb(" + this.red + "," + this.green + "," + this.blue + ")"
     }
+    this.updateDisplay = function() {
+      this.display.style.backgroundColor = this.color()
+    }
     this.scramble = function() {
       this.red = Math.floor(Math.random() * 255)
       this.green = Math.floor(Math.random() * 255)
       this.blue = Math.floor(Math.random() * 255)
-      this.display.style.backgroundColor = this.color()
+      this.updateDisplay()
     }
     this.scramble()
     this.showHide = function() {
@@ -29,10 +45,12 @@ const colorButton = function(name, id, display) {
     this.display.addEventListener("click", boxClicked.bind(this))
 }
 
+// Declare the onscreen boxes as colorButtons, based on our definition
 for (let i = 0; i < 6; i++) {
-    box[i] = new colorButton("box" + i, "c" + i, document.querySelector("#c" + i))
+    box[i] = new colorButton("box" + i, document.querySelector("#c" + i))
 }
 
+// Definition for the color number players will compare the boxes to
 const targetNum = function(redDisplay, greenDisplay, blueDisplay) {
   this.red = null
   this.redDisplay = redDisplay
@@ -56,17 +74,17 @@ const targetNum = function(redDisplay, greenDisplay, blueDisplay) {
   }
   this.scramble()
   this.colorBox = function(item) {
-      item.display.style.backgroundColor = this.color()
+      item.red = this.red
+      item.green = this.green
+      item.blue = this.blue
+      item.updateDisplay()
   }
 }
 
+// Defining the three onscreen numbers as the target number
 let myTargetNum = new targetNum(document.querySelector("#redCode"),
                                 document.querySelector("#greenCode"),
                                 document.querySelector("#blueCode"))
 
-const randomBox = function(array) {
-    let num = Math.floor(Math.random() * array.length)
-    return array[num]
-}
-
+// Recolors one of the boxes onscreen to match the target number
 myTargetNum.colorBox(randomBox(box))
